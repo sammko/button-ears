d9f7bd3851929ce06d186aebee4c8b0c3ac8c122
sammko <samuel@cavoj.net>
2024-09-07 18:25:58 +0100

    Re-add devices when they reappear

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use anyhow::{bail, Context, Error};
 use argh::FromArgs;
 use async_udev::AsyncMonitorSocket;
-use evdev::{InputEventKind, Key};
+use evdev::{EventStream, InputEventKind, Key};
 use tokio::{process::Command, select};
 use tokio_stream::{StreamExt, StreamMap};
 use udev::{Enumerator, MonitorBuilder};
@@ -11,7 +11,7 @@ use udev::{Enumerator, MonitorBuilder};
 mod async_udev;
 
 const UDEV_TAG: &str = "button-ears";
-const TARGET_KEYS: &[Key] = &[Key::KEY_SLEEP, Key::KEY_SUSPEND, Key::KEY_POWER, Key::KEY_B];
+const TARGET_KEYS: &[Key] = &[Key::KEY_SLEEP, Key::KEY_SUSPEND, Key::KEY_POWER];
 
 #[derive(FromArgs)]
 /// Listen for power button events
@@ -19,6 +19,34 @@ struct ButtonEars {
     #[argh(positional)]
     /// program to execute
     command: String,
+}
+
+fn add_to_stream_map(
+    stream_map: &mut StreamMap<PathBuf, EventStream>,
+    udev: &udev::Device,
+) -> Result<(), Error> {
+    let devpath = Path::new(udev.devpath()).to_path_buf();
+    let Some(devnode) = udev.devnode() else {
+        bail!("Device has no devnode: {}", devpath.display());
+    };
+    let evdev = evdev::Device::open(devnode)
+        .with_context(|| format!("Could not open device {}", devnode.display()))?;
+    if let Some(supported_keys) = evdev.supported_keys() {
+        if !TARGET_KEYS.iter().any(|key| supported_keys.contains(*key)) {
+            bail!(
+                "Device {} does not support any of TARGET_KEYS",
+                devnode.display()
+            )
+        }
+    }
+    let stream = evdev
+        .into_event_stream()
+        .with_context(|| format!("Could not make stream for dev {}", devpath.display()))?;
+
+    eprintln!("Adding device {}", devpath.display());
+    stream_map.insert(devpath, stream);
+
+    Ok(())
 }
 
 #[tokio::main]
@@ -33,25 +61,10 @@ async fn main() -> Result<(), Error> {
         .scan_devices()
         .context("Could not enumerate udev devices")?
     {
-        let devpath = Path::new(udev.devpath()).to_path_buf();
-        let Some(devnode) = udev.devnode() else {
-            eprintln!("Device has no devnode: {}", devpath.display());
-            continue;
-        };
-        let evdev = evdev::Device::open(devnode)
-            .with_context(|| format!("Could not open device {}", devnode.display()))?;
-        if let Some(supported_keys) = evdev.supported_keys() {
-            if !TARGET_KEYS.iter().any(|key| supported_keys.contains(*key)) {
-                bail!(
-                    "Device {} does not support any of TARGET_KEYS",
-                    devnode.display()
-                )
-            }
+        if let Err(err) = add_to_stream_map(&mut stream_map, &udev).context("Failed to add device")
+        {
+            eprintln!("{:?}", err);
         }
-        let stream = evdev
-            .into_event_stream()
-            .with_context(|| format!("Could not make stream for dev {}", devpath.display()))?;
-        stream_map.insert(devpath, stream);
     }
 
     let mut monitor: AsyncMonitorSocket = MonitorBuilder::new()?
@@ -60,15 +73,17 @@ async fn main() -> Result<(), Error> {
         .try_into()?;
 
     loop {
-        eprintln!("loop");
         select! {
-            Some(ev_udev) = monitor.next() => {
-                eprintln!("Got udev event: {ev_udev:?}")
-                // TODO add to stream_map
+            Some(ev) = monitor.next() => {
+                let ev = ev.context("Udev monitor died")?;
+                if ev.event_type() == udev::EventType::Add {
+                    if let Err(err) = add_to_stream_map(&mut stream_map, &ev.device()).context("Failed to add device") {
+                        eprintln!("{:?}", err)
+                    }
+                }
             },
-            Some((key, ev_input)) = stream_map.next(), if !stream_map.is_empty() => {
-                eprintln!("Got input event: {ev_input:?}");
-                match ev_input {
+            Some((key, ev)) = stream_map.next(), if !stream_map.is_empty() => {
+                match ev {
                     Ok(ev) => {
                         let ev_kind = ev.kind();
                         if TARGET_KEYS
@@ -81,7 +96,7 @@ async fn main() -> Result<(), Error> {
                                 Err(e) => {
                                     eprintln!("Command failed: {e}")
                                 }
-                           }
+                            }
                         }
                     },
                     Err(_) => {
